75f7c3c904a010befa0030cceaa15e1e98acd6d0
RegMCS <33059228+RegMCS@users.noreply.github.com>
2024-11-11 20:36:22 +0800

    router update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -26,7 +26,7 @@ import NotFound from '@/components/NotFound.vue';
 
 const routes = [
     {
-        path: '/index.html',
+        path: '/',
         name: 'Home',
         component: HomePage,
     },
